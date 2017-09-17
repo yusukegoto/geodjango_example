b082a0c12cdff9bb21f1cb358f424bf7a37d7bf9
yusuke.goto.a <yusuke.goto.a@hunza.jp>
2017-09-17 21:11:29 +0900

    add __str__ method

--- geo/models.py
+++ geo/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=255, null=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Branch(models.Model):
     name = models.CharField(max_length=255, null=False)
     restaurant = models.ForeignKey(Restaurant, null=False)
+
+    def __str__(self):
+        return self.name
